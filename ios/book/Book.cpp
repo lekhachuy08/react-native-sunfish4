a4e3b66fe64271b2a400e0b0d1ec1c4cbbc6c785
Huy Le <lekhachuy08@gmail.com>
2018-08-11 11:15:40 +1000

    Allow engine to use book

--- ios/book/Book.cpp
+++ ios/book/Book.cpp
@@ -7,147 +7,110 @@
 #include "../core/position/Position.hpp"
 #include "../core/record/SfenParser.hpp"
 #include "../common/string/StringUtil.hpp"
+#include "../bookbin.hpp"
 #include <fstream>
+#include <sstream>
 #include <cstring>
 
 namespace {
-
-const char* const BookBin = "../book.bin";
-
+	
+	const char* const BookBin = "book.bin";
+	
 } // namespace
 
 namespace sunfish {
-
-void Book::insert(const Position& position, Move move, int count) {
-  std::string sfen = position.toStringSFEN();
-  auto ite = map_.find(sfen);
-  if (ite == map_.end()) {
-    map_[sfen].push_back({ move, static_cast<uint16_t>(count) });
-    return;
-  }
-
-  auto& bookMoves = ite->second;
-
-  for (auto& bookMove : bookMoves) {
-    if (bookMove.move == move) {
-      bookMove.count += static_cast<uint16_t>(count);
-      return;
-    }
-  }
-
-  bookMoves.push_back({ move, static_cast<uint16_t>(count) });
-}
-
-void Book::sort() {
-  for (auto& pair : map_) {
-    std::sort(pair.second.begin(), pair.second.end(), [](BookMove lhs, BookMove rhs) {
-      return lhs.count >= rhs.count;
-    });
-  }
-}
-
-const BookMoves* Book::get(const Position& position) const {
-  std::string sfen = position.toStringSFEN();
-  auto ite = map_.find(sfen);
-  return ite != map_.end() ? &ite->second : nullptr;
-}
-
-bool Book::load() {
-  std::ifstream file(BookBin);
-  if (!file) {
-    LOG(error) << "could not open a file: " << BookBin;
-    return false;
-  }
-
-  bool ok = load(file);
-
-  file.close();
-
-  return ok;
-}
-
-bool Book::load(std::istream& is) {
-  std::string buffer;
-  Position pos;
-  bool isPosInitialized = false;
-
-  while (true) {
-    getline(is, buffer);
-
-    if (is.eof()) {
-      return true;
-    }
-
-    if (is.fail()) {
-      LOG(error) << "file I/O error";
-      return false;
-    }
-
-    if (strncmp(buffer.c_str(), "sfen ", 5) == 0) {
-      bool ok = SfenParser::parsePosition(buffer.c_str() + 5, pos);
-      if (!ok) {
-        return false;
-      }
-
-      isPosInitialized = true;
-
-    } else {
-      if (!isPosInitialized) {
-        LOG(error) << "position is required before moves";
-        return false;
-      }
-
-      auto columns = StringUtil::split(buffer, ' ');
-      if (columns.size() < 2) {
-        LOG(error) << "invalid format";
-        return false;
-      }
-
-      Move move;
-      bool ok = SfenParser::parseMove(columns[0], move);
-      if (!ok) {
-        return false;
-      }
-
-      if (move.isNone()) {
-        LOG(error) << "first column is required: " << buffer;
-        return false;
-      }
-
-      int count = std::stoi(columns[1]);
-
-      insert(pos, move, count);
-
-    }
-  }
-}
-
-bool Book::save() const {
-  std::ofstream file(BookBin);
-  if (!file) {
-    LOG(error) << "could not open a file: " << BookBin;
-    return false;
-  }
-
-  bool ok = save(file);
-
-  file.close();
-
-  return ok;
-}
-
-bool Book::save(std::ostream& os) const {
-  for (const auto& pair : map_) {
-    auto& sfen = pair.first;
-    auto& bookMoves = pair.second;
-
-    os << "sfen " << sfen << '\n';
-    for (auto& bookMove : bookMoves) {
-      os << bookMove.move.toStringSFEN() << ' ' << bookMove.count << '\n';
-    }
-  }
-
-  return true;
-}
-
+	
+	void Book::insert(const Position& position, Move move, int count) {
+		std::string sfen = position.toStringSFEN();
+		auto ite = map_.find(sfen);
+		if (ite == map_.end()) {
+			map_[sfen].push_back({ move, static_cast<uint16_t>(count) });
+			return;
+		}
+		
+		auto& bookMoves = ite->second;
+		
+		for (auto& bookMove : bookMoves) {
+			if (bookMove.move == move) {
+				bookMove.count += static_cast<uint16_t>(count);
+				return;
+			}
+		}
+		
+		bookMoves.push_back({ move, static_cast<uint16_t>(count) });
+	}
+	
+	void Book::sort() {
+		for (auto& pair : map_) {
+			std::sort(pair.second.begin(), pair.second.end(), [](BookMove lhs, BookMove rhs) {
+				return lhs.count >= rhs.count;
+			});
+		}
+	}
+	
+	const BookMoves* Book::get(const Position& position) const {
+		std::string sfen = position.toStringSFEN();
+		auto ite = map_.find(sfen);
+		return ite != map_.end() ? &ite->second : nullptr;
+	}
+	
+	bool Book::load() {
+		std::string buffer;
+		Position pos;
+		
+		for (int i = 0; i < sizeof(book) / sizeof(book[0]); i++) {
+			buffer = book[i];
+			
+			if (strncmp(buffer.c_str(), "sfen ", 5) == 0) {
+				bool ok = SfenParser::parsePosition(buffer.c_str() + 5, pos);
+				if (!ok) {
+					return false;
+				}
+			} else {
+				auto columns = StringUtil::split(buffer, ' ');
+				
+				Move move;
+				bool ok = SfenParser::parseMove(columns[0], move);
+				if (!ok) {
+					return false;
+				}
+				
+				int count = std::stoi(columns[1]);
+				
+				insert(pos, move, count);
+				
+			}
+		}
+		
+		return true;
+	}
+	
+	bool Book::save() const {
+		std::ofstream file(BookBin);
+		if (!file) {
+			LOG(error) << "could not open a file: " << BookBin;
+			return false;
+		}
+		
+		bool ok = save(file);
+		
+		file.close();
+		
+		return ok;
+	}
+	
+	bool Book::save(std::ostream& os) const {
+		for (const auto& pair : map_) {
+			auto& sfen = pair.first;
+			auto& bookMoves = pair.second;
+			
+			os << "sfen " << sfen << '\n';
+			for (auto& bookMove : bookMoves) {
+				os << bookMove.move.toStringSFEN() << ' ' << bookMove.count << '\n';
+			}
+		}
+		
+		return true;
+	}
+	
 } // namespace sunfish
--- ios/book/Book.hpp
+++ ios/book/Book.hpp
@@ -26,39 +26,37 @@ using BookMoves = std::vector<BookMove>;
 
 class Book {
 public:
-
-  using BookMap = std::unordered_map<std::string, BookMoves>;
-
+	
+	using BookMap = std::unordered_map<std::string, BookMoves>;
+	
 public:
-
-  void insert(const Position& position, Move move) {
-    insert(position, move, 1);
-  }
-
-  void insert(const Position& position, Move move, int count);
-
-  void sort();
-
-  const BookMoves* get(const Position& position) const;
-
-  void clear() {
-    map_.clear();
-  }
-
-  bool load();
-
-  bool load(std::istream& is);
-
-  bool save() const;
-
-  bool save(std::ostream& os) const;
-
+	
+	void insert(const Position& position, Move move) {
+		insert(position, move, 1);
+	}
+	
+	void insert(const Position& position, Move move, int count);
+	
+	void sort();
+	
+	const BookMoves* get(const Position& position) const;
+	
+	void clear() {
+		map_.clear();
+	}
+	
+	bool load();
+	
+	bool save() const;
+	
+	bool save(std::ostream& os) const;
+	
 private:
-
-  BookMap map_;
-
+	
+	BookMap map_;
+	
 };
-
+	
 } // namespace sunfish
 
 #endif // SUNFISH_BOOK_BOOK_HPP__
